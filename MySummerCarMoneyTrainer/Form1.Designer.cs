0eb5b33b40a098f19d1b24e7c68aee6753efeb6b
David <cesarovic@seznam.cz>
2016-12-24 00:27:49 +0100

    Made windows unresizable

--- MySummerCarMoneyTrainer/Form1.Designer.cs
+++ MySummerCarMoneyTrainer/Form1.Designer.cs
@@ -73,6 +73,7 @@
             this.Controls.Add(this.button_Set);
             this.Controls.Add(this.textBox1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
